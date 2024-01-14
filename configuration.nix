fa1d8f2d4e31295057edeb65f49b34d91654c218
Olivier "Wono" Wonovith <wono@wono.fr>
2024-01-14 23:16:26 +0100

    extracted common options to common.nix

--- configuration.nix
+++ configuration.nix
@@ -2,62 +2,15 @@
 let nur = config.nur;
 in {
     imports = [
+        ./common.nix
         ./hardware-configuration.nix
         ./tuxedo.nix
     ];
 
-    nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-    ];
-
-    nixpkgs.config.allowUnfree = true;
-
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
     networking.hostName = "aura";
-    networking.networkmanager.enable = true;
-
-    time.timeZone = "Europe/Paris";
-
-    i18n.defaultLocale = "fr_FR.UTF-8";
-
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "fr_FR.UTF-8";
-        LC_IDENTIFICATION = "fr_FR.UTF-8";
-        LC_MEASUREMENT = "fr_FR.UTF-8";
-        LC_MONETARY = "fr_FR.UTF-8";
-        LC_NAME = "fr_FR.UTF-8";
-        LC_NUMERIC = "fr_FR.UTF-8";
-        LC_PAPER = "fr_FR.UTF-8";
-        LC_TELEPHONE = "fr_FR.UTF-8";
-        LC_TIME = "fr_FR.UTF-8";
-    };
-
-    services.xserver.enable = true;
 
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
-
-    services.xserver = {
-        layout = "fr";
-        xkbVariant = "";
-    };
-
-    console.keyMap = "fr";
-
-    services.printing.enable = true;
-
-    sound.enable = true;
-        hardware.pulseaudio.enable = false;
-        security.rtkit.enable = true;
-        services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-    };
 
     users.users.w = {
         shell = pkgs.zsh;
